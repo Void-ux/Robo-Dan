4a4d455e09f8f032e250e8f5f94ad20668e661d8
Void-ux <77840397+Void-ux@users.noreply.github.com>
2025-03-13 23:54:45 +0000

    fix: bugs 😎

--- cogs/hot_reload.py
+++ cogs/hot_reload.py
@@ -14,7 +14,7 @@ from utils.context import Context
 
 log = logging.getLogger(__name__)
 
-IGNORE_EXTENSIONS = ('jishaku', )
+IGNORE_EXTENSIONS = ('jishaku', 'cogs.hot_reload')
 
 
 def colour_format(m) -> str:
@@ -38,6 +38,11 @@ class LazyHotReload(commands.Cog):
     to ensure the latest code is actively ran.
 
     TL:DR; saves you from having to `-reload` during development.
+
+    Warning
+    -------
+    - This does NOT *load* modules when placed into `cogs/`, duh.
+    - This extension excludes itself from reloads
     """
 
     def __init__(self, bot: commands.Bot):
@@ -48,7 +53,7 @@ class LazyHotReload(commands.Cog):
         self.bot.tree.interaction_check = self.interaction_check  # pyright: ignore
 
     async def _populate(self):
-        await asyncio.sleep(3)
+        await self.bot.wait_until_ready()
 
         for ext in self.bot.extensions.keys():
             if ext in IGNORE_EXTENSIONS:
@@ -60,7 +65,11 @@ class LazyHotReload(commands.Cog):
             self.last_modified_times[ext] = last_modified
 
     async def reload_extension(self, extension: str) -> bool:
-        """Reload a single extension."""
+        """Reload a single extension.
+        
+        Returns
+        -------
+        :class:`bool` True if successful, False otherwise"""
         last_modified = get_last_modified(extension)
         
         if last_modified is None:  # builtins
@@ -70,69 +79,77 @@ class LazyHotReload(commands.Cog):
             log.info('Found an update to %s; performing update', extension)
             await self.bot.reload_extension(extension)
         except commands.ExtensionError:
-            log.info('Unable to load %s', extension)
+            log.error('Unable to load %s', extension)
             return False
         else:
             self.last_modified_times[extension] = last_modified
             log.info('Reloaded extension: %s', extension)
             return True
 
-    async def reload_extensions(self) -> None:
-        """Reload all extensions present in the cogs/ directory."""
-        for ext in Path('cogs').iterdir():
-            await self.reload_extension(f'{__name__}.{ext}')
-
-    async def _process_command(self, ctx: Context) -> None:
-        """Checks a command's module for pending updates, and if yes, applies them and then re-invokes."""
+    async def _process_command(self, ctx: Context) -> bool:
+        """Checks a command's module for pending updates, and if yes, applies them and then re-invokes.
+        
+        Returns
+        -------
+        :class:`bool`
+            True if command execution is to proceed, False when reload is required.
+        """
         assert ctx.command
         ext = ctx.command.module
         if ext not in self.bot.extensions:
            log.info('%s is not being tracked.', ext)
-           return
+           return True
 
         last_modified = get_last_modified(ext)
         if self.last_modified_times.get(ext) is None:
-            log.info('Unknown module {}; will continue to monitor.', ext)
-            return
+            log.info('Unknown module %s; will continue to monitor.', ext)
+            return True
 
         if last_modified is None or last_modified <= self.last_modified_times[ext]:
             log.info('Skipping %s; already up to date.', ext)
-            return
+            return True
 
-        fail = await self.reload_extension(ext)
-        if fail:
-            log.warning('Unable to reload extension %s', ext)
-            return
+        success = await self.reload_extension(ext)
+        if not success:
+            return True
 
         command = self.bot.get_command(ctx.command.qualified_name)
         if command is None:
             log.info('Command not found.')
-            return
+            return True
+
+        ctx.command = command
+        self.last_modified_times[ext] = last_modified
+        log.debug('Re-invoking command with updated callback.')
+        await self.bot.invoke(ctx)
+        if discord.utils.is_docker():
+            await asyncio.create_task(
+                ctx.message.add_reaction(discord.PartialEmoji.from_str('<:restart:1349890490149113896>'))
+            )
+        return False
 
     async def lazy_reload(self, ctx: Context) -> bool:
         """Handles hot reloading prior to text invokation."""
-        await self._process_command(ctx)
-        return True
+        return await self._process_command(ctx)
 
-    async def interaction_check(  # pyright: ignore
+    async def interaction_check(
         self,
         interaction: Interaction,
         /
     ) -> bool:
         """Serves to block command invcation until its module/cog is up to date.
-        Wraps around other pre-defined `interaction_check` to facilitate reloading,
-        even when invoked via app command.
-        """
-        if interaction.type != discord.InteractionType.application_command:
-            return True
 
+        Wraps around other pre-defined `interaction_check` to facilitate reloading, even when invoked via app command.
+        """
         failure = await self._old_tree_check(interaction)  # pyright: ignore
         if failure:
             return False
 
+        if interaction.type != discord.InteractionType.application_command:
+            return True
+
         ctx = await Context.from_interaction(interaction)
-        await self._process_command(ctx)
-        return True
+        return await self._process_command(ctx)
 
     @commands.hybrid_command()
     @commands.is_owner()
@@ -178,10 +195,10 @@ class LazyHotReload(commands.Cog):
             if not ext.name.endswith('.py') or ext.name.startswith('__'):
                 continue
 
-            if f'cogs.{ext.name.replace('.py', '')}' in self.bot.extensions:
-                text = f'<:tick:1349188008028672020> **Active** `cogs.{ext.name}`'
+            if f'cogs.{(ext := ext.name.replace('.py', ''))}' in self.bot.extensions:
+                text = f'<:tick:1349188008028672020> **Active** `cogs.{ext}`'
             else:
-                text = f'<:cross:1349188010016768071> **Inactive** `cogs.{ext.name}`'
+                text = f'<:cross:1349188010016768071> **Inactive** `cogs.{ext}`'
 
             rows.append(text)
             
